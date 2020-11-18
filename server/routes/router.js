f1168d423b778caf834142742d57be62add4ec0b
Jose Valadez <vinmajj@gmail.com>
2020-11-18 11:52:31 -0800

    update validateLogin middleware method

--- server/routes/router.js
+++ server/routes/router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { createUser, getUser } = require("../controllers/index");
 const middleware = require("../middleware/users");
 
-router.post("/login", middleware.validateRegister, getUser.post);
+router.post("/login", middleware.validateLogin, getUser.post);
 router.post("/sign-up",  middleware.validateRegister, createUser.post);
 router.get("/secrete-route", (req, res, next) => {
   res.send("This is the secret content. Only logged in users can see that!");
