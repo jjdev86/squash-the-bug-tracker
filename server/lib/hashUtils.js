09962e1038a5a2ebe91a79da8caad2685cf2f951
Jose Valadez <vinmajj@gmail.com>
2020-11-17 23:06:06 -0800

    create hash pass hash lib

---
+++ server/lib/hashUtils.js
@@ -0,0 +1,16 @@
+const crypto = require('crypto');
+const csprng = require('csprng');
+
+
+module.exports = {
+    hash :  (pwd) => {
+        return crypto
+            .createHash('sha256')
+            .update(pwd)
+            .digest('base64')
+    },
+    salt : () => csprng(160, 36),
+    validatePassword: (pass, hash) => {
+
+    }
+} 
