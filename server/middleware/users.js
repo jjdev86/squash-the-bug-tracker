16e3d4592023967e3ba170f90ee66a81727a63b2
Jose Valadez <vinmajj@gmail.com>
2020-11-18 13:47:02 -0800

    add isLoggedIn method, verify jwt from frontend'

--- server/middleware/users.js
+++ server/middleware/users.js
@@ -1,3 +1,5 @@
+const dotenv = require("dotenv").config();
+const jwt = require("jsonwebtoken");
 module.exports = {
   // vaidate email
   validateRegister: (req, res, next) => {
@@ -10,7 +12,7 @@ module.exports = {
     // password min 6 chars
     if (!req.body.password || req.body.password.length < 5) {
       return res.status(400).send({
-        msg: "Please enter a password with min. 5 chars"
+        msg: "Please enter a password with min. 5 chars",
       });
     }
 
@@ -25,15 +27,27 @@ module.exports = {
   validateLogin: (req, res, next) => {
     if (!req.body.email || req.body.email.length < 3) {
       return res.status(400).send({
-        msg: 'Please enter a valid email address'
-      })
+        msg: "Please enter a valid email address",
+      });
     }
 
     if (!req.body.password || req.body.password.length < 5) {
       return res.status(400).send({
-        msg: 'Please enter a password with min. 5 chars'
-      })
+        msg: "Please enter a password with min. 5 chars",
+      });
     }
     next();
-  }
+  },
+  isLoggedIn: (req, res, next) => {
+    try {
+      const token = req.headers.authorization.split(" ");
+      const decoded = jwt.verify(token, process.env.JWT_SECRET_TOKEN);
+      req.userData = decoded;
+      next();
+    } catch (err) {
+      return res.status(401).send({
+        msg: "Your session is not valid!",
+      });
+    }
+  },
 };
