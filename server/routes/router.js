8b423409270bbd9f48dc1557033d6d14508595e2
Jose Valadez <vinmajj@gmail.com>
2020-11-17 23:07:52 -0800

    create routes

---
+++ server/routes/router.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const { createUser, getUser } = require("../controllers/index");
+const middleware = require("../middleware/users");
+
+router.post("/login", middleware.validateRegister, getUser.post);
+router.post("/sign-up",  middleware.validateRegister, createUser.post);
+router.get("/secrete-route", (req, res, next) => {
+  res.send("This is the secret content. Only logged in users can see that!");
+});
+
+module.exports = router;
