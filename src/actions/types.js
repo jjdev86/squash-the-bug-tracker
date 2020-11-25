8ba78b876be856d9dd6b3b4d3ea3044bc9412db6
Jose Valadez <vinmajj@gmail.com>
2020-11-24 16:16:39 -0800

    create auth actions

---
+++ src/actions/types.js
@@ -0,0 +1,9 @@
+
+export const REGISTER_SUCESS = "REGISTER_SUCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
+export const LOGIN_SUCESS = 'LOGIN_SUCESS';
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOG_OUT = "LOG_OUT";
+
+export const SET_MESSAGE = "SET_MESSAGE";
+export const CLEAR_MESSAGE = "CLEAAR_MESSAGE";
