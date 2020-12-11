96e51cb2727f168b7d904e321a20c956efb7fba2
Jose Valadez <vinmajj@gmail.com>
2020-12-10 16:42:21 -0800

    add middleware function to router

--- server/routes/router.js
+++ server/routes/router.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const { createUser, getUser } = require("../controllers/index");
 const middleware = require("../middleware/users");
 
-router.post("/login", middleware.validateLogin, getUser.post);
+router.post("/login", getUser.post);
 router.post("/sign-up",  middleware.validateRegister, createUser.post);
-router.get("/secrete-route", (req, res, next) => {
+router.get("/secrete-route", middleware.isLoggedIn, (req, res, next) => {
   res.send("This is the secret content. Only logged in users can see that!");
 });
 
