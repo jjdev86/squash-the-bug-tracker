41d29e77f2960ce477c9e9905824e4dfae50af28
Jose Valadez <vinmajj@gmail.com>
2020-12-10 16:39:01 -0800

    clean code

--- server/lib/hashUtils.js
+++ server/lib/hashUtils.js
@@ -4,14 +4,10 @@ const csprng = require('csprng');
 
 module.exports = {
     hash :  (pwd) => {
-        console.log(pwd, `in hash`)
         return crypto
             .createHash('sha256')
             .update(pwd)
             .digest('base64')
     },
     salt : () => csprng(160, 36),
-    validatePassword: (pass, hash) => {
-        
-    }
 } 
