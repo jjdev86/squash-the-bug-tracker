e71c76218fa2added7fb2a5332bd19e1657be612
Jose Valadez <vinmajj@gmail.com>
2020-12-10 16:46:10 -0800

    minor change

--- src/actions/types.js
+++ src/actions/types.js
@@ -6,4 +6,4 @@ export const LOGIN_FAIL = "LOGIN_FAIL";
 export const LOG_OUT = "LOG_OUT";
 
 export const SET_MESSAGE = "SET_MESSAGE";
-export const CLEAR_MESSAGE = "CLEAAR_MESSAGE";
+export const CLEAR_MESSAGE = "CLEAR_MESSAGE";
