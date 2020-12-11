baadf70f292973a9f14b706b1de24cc21513ace5
Jose Valadez <vinmajj@gmail.com>
2020-12-10 16:49:27 -0800

    change fetch for axios lib

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,84 +1,75 @@
 // login user
-import * as actionTypes from '../../../store/actions';
+import axios from 'axios';
 
+const API_URL = 'http://localhost:3000/api/'
 
- const login = (userInfo, history) => {
+ const login = ({email, password}) => {
 
-  fetch("/api/login", {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json",
-    },
-    body: JSON.stringify(userInfo),
-  }).then((res) => {
-    if (!res.ok) {
-      if (res.status === 409 || res.status === 500) {
-        throw res;
-      }
-    } else {
-      return res.json();
-    }
-  })
-  .then(data => {
-    localStorage.setItem("token", data.token);
-    // dispatch({type: actionTypes.SET_USER, payload: data.user});
-    // history.push('/dashboard');
-    return data;
-  })
+  return axios.post(API_URL + 'login', {
+    email,
+    password
+  });
 
-  .catch(err => {
-    if (err.status)
-    if (err.text) {
-      err.text().then((serverError) => {
-        console.log(JSON.parse(serverError), `error message`);
-      });
-    } else {
-      console.log("hard code error");
-    }
-  })
+  //  return fetch("/api/login", {
+  //   method: "POST",
+  //   mode: "cors",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     "Accept": "application/json",
+  //   },
+  //   body: JSON.stringify(userInfo)
+  // })
 
 };
 // create new user
- const register = (userInfo, history) => {
-  fetch('/api/sign-up', {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json",
-    },
-    body: JSON.stringify(userInfo)
-  })
-  .then(res => {
-    if (!res.ok) {
-      if (res.status === 409 || res.status === 500 || res.status === 400) {
-        throw res;
-      }
-    } else {
-      return res.json();
-    }
-  })
-  .then(data => {
-    localStorage.setItem("token", data.token);
-    // dispatch({type: actionTypes.SET_USER, payload: data.user});
-    return data;
-    // history.push('/dashboard');
-  })
-  .catch(err => {
-    if (err.status)
-    if (err.text) {
-      err.text().then((serverError) => {
-        console.log(JSON.parse(serverError), `error message`);
-      });
-    } else {
-      console.log("hard code error");
-    }
-  })
 
+const register = ({email, password, confPassword}) => {
+  return axios.post(API_URL + 'sign-up', {
+    email,
+    password,
+    confPassword
+  });
 
 };
+
+//  const register = (userInfo) => {
+//   fetch('/api/sign-up', {
+//     method: "POST",
+//     mode: "cors",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify(userInfo)
+//   })
+//   .then(res => {
+//     if (!res.ok) {
+//       if (res.status === 409 || res.status === 500 || res.status === 400) {
+//         throw res;
+//       }
+//     } else {
+//       return res.json();
+//     }
+//   })
+//   .then(data => {
+//     localStorage.setItem("token", data.token);
+//     // dispatch({type: actionTypes.SET_USER, payload: data.user});
+//     return data;
+//     // history.push('/dashboard');
+//   })
+//   .catch(err => {
+//     if (err.status)
+//     if (err.text) {
+//       err.text().then((serverError) => {
+//         console.log(JSON.parse(serverError), `error message`);
+//       });
+//     } else {
+//       console.log("hard code error");
+//     }
+//   })
+
+
+// };
 // keep user login
  const autoLogin = () => (dispatch) => {
   fetch('/auto_login', {
